6b575086438a386cf87759cba95188e043d637ec
enidev911 <enidev911@gmail.com>
2024-08-13 16:05:36 -0400

    change: add flan model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Flan(models.Model):
+  flan_uuid = models.UUIDField(default=uuid.uuid4)
+  name = models.CharField(max_length=50)
+  description = models.TextField()
+  image_url = models.URLField()
+  slug = models.SlugField()
+  is_private = models.BooleanField()
+  
+  def __str__(self):
+    return f"{self.name}:{self.is_private}"
