b9450aa6973408ec0141d8db3c610d00c9fb2de0
enidev911 <enidev911@gmail.com>
2024-08-13 15:05:38 -0400

    change: remove funtions and imports

--- web/views.py
+++ web/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def indice(request):
-    return render(request, 'index.html', {})
+	return render(request, 'index.html')
 
 def acerca(request):
     return render(request, 'about.html', {})
