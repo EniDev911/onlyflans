47ff0180333c610b64689045985f87c4796860e8
enidev911 <enidev911@correo.com>
2024-08-14 01:47:39 -0400

    change: filter objects in each view

--- web/views.py
+++ web/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Flan
 
-# Create your views here.
 def indice(request):
-	return render(request, 'index.html')
+    flanes_publicos = Flan.objects.filter(is_private=False)
+    return render(request, 'index.html', {
+        'flanes_publicos': flanes_publicos
+    })
 
 def acerca(request):
     return render(request, 'about.html', {})
 
 def bienvenido(request):
-    return render(request, 'welcome.html', {})
+    flanes_privados = Flan.objects.filter(is_private=True)
+    return render(request, 'welcome.html', {
+        'flanes_privados': flanes_privados
+    })
