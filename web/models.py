0091dc6ed221776bc4954033f6f657fbbc60890c
enidev911 <enidev911@correo.com>
2024-08-14 02:38:33 -0400

    change: update model Flan, add params

--- web/models.py
+++ web/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 import uuid
+from django.utils.text import slugify
 
 class Flan(models.Model):
-  flan_uuid = models.UUIDField(default=uuid.uuid4)
+  flan_uuid = models.UUIDField(default=uuid.uuid4, editable=False)
   name = models.CharField(max_length=50)
   description = models.TextField()
   image_url = models.URLField()
-  slug = models.SlugField()
+  slug = models.SlugField(max_length=200, unique=True, blank=True)
   is_private = models.BooleanField()
   
+  def save(self, *args, **kwargs):
+    if not self.slug:
+      self.slug = slugify(self.name)
+    super(Flan, self).save(*args, **kwargs)
+
   def __str__(self):
     return f"{self.name}:{self.is_private}"
