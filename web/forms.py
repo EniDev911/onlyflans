87a0ecd2922ee0c8a4cc0a0e54b31abff7c78b85
enidev911 <enidev911@gmail.com>
2024-08-17 02:21:18 -0400

    change: add more attributes in form

--- web/forms.py
+++ web/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
 class ContactFormForm(forms.Form):
-	customer_email = forms.EmailField(label="Correo")
-	customer_name = forms.CharField(max_length=64, label="Nombre")
-	message = forms.CharField(label="Mensaje")
+	customer_email = forms.EmailField(label="Correo", widget=forms.EmailInput(
+		attrs={
+			"class": "form-control",
+			"placeholder": "xyz@mail.com"
+		}
+	))
+	customer_name = forms.CharField(max_length=64, label="Nombre", widget=forms.TextInput(
+		attrs={
+			"class": "form-control",
+			"placeholder": "john doe"
+		}
+	))
+	message = forms.CharField(label="Mensaje", widget=forms.Textarea(
+		attrs={
+			"rows": 4,
+			"class": "form-control",
+			"placeholder": "Escribenos tu experiencia o recomendación"
+		}
+	))
