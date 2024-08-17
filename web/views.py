b6b0f0c619528e9f3ade30f8e8121914166d373c
enidev911 <enidev911@gmail.com>
2024-08-16 21:45:29 -0400

    change: add view contacto

--- web/views.py
+++ web/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from .models import Flan
+from django.shortcuts import render, redirect
+from .forms import ContactFormForm
+from .models import ContactForm, Flan
 
 def indice(request):
     flanes_publicos = Flan.objects.filter(is_private=False)
@@ -15,3 +16,17 @@ def bienvenido(request):
     return render(request, 'welcome.html', {
         'flanes_privados': flanes_privados
     })
+
+def contacto(request):
+    if request.method == "POST":
+        form = ContactFormForm(request.POST)
+        if form.is_valid():
+            ContactForm(**form.cleaned_data).save()
+            return redirect('indice')
+    else:
+        form = ContactFormForm()
+    
+    return render(request, 'contact.html', {
+        'form': form
+    })
+        
