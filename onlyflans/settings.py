70face0ca42b4b0848004f8a576e95aed3f79b7e
enidev911 <enidev911@gmail.com>
2024-08-21 19:55:11 -0400

    change: add redirect default settings login and logout

--- onlyflans/settings.py
+++ onlyflans/settings.py
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redireccionar Después de Iniciar Sesión
+LOGIN_REDIRECT_URL = "/bienvenido"
+# Redireccionar Después de Cerrar Sesión
+LOGOUT_REDIRECT_URL = '/'
