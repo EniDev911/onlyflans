ddde33985294cc851d6449cee44557b868888dbc
enidev911 <enidev911@gmail.com>
2024-08-30 05:13:59 -0400

    change: add view to register user

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .forms import ContactFormForm
+from django.contrib.auth import login
+from .forms import ContactFormForm, UserRegistrationForm
 from .models import ContactForm, Flan
 
 def indice(request):
@@ -37,3 +38,17 @@ def exito(request):
 def detalle(request, id):
     flan = get_object_or_404(Flan, id=id)
     return render(request, 'detail.html', {'flan': flan})
+
+def registrar_usuario(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            login(request, user)
+            return redirect('indice')
+    else:
+        form = UserRegistrationForm()
+    
+    return render(request, 'register.html', {'form': form})
